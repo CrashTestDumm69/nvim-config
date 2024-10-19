2ead34fde1cb15dbf94ae7e6e891e9317d51678f
Vishnu <nvishnuh@gmail.com>
2024-10-19 09:16:54 +0530

    Neovide config added

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("crash.core")
 require("crash.lazy")
+
+if vim.g.neovide then
+    vim.g.neovide_transparency = 0.8
+    vim.g.neovide_cursor_trail_size = 0.85
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
