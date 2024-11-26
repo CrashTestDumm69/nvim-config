870a96f96e771863c2fbe72adf950f4e6b26d4c2
Vishnu <nair.vishnuh@icloud.com>
2024-11-26 14:40:08 +0530

    Added Font

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("crash.core")
 require("crash.lazy")
 
 if vim.g.neovide then
+    vim.o.guifont = "Mononoki Nerd Font:h14"
     vim.g.neovide_transparency = 0.8
     vim.g.neovide_cursor_trail_size = 0.85
     vim.g.neovide_hide_mouse_when_typing = true
